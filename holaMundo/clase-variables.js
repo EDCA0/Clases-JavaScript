4b81e117ae03b84d9ce3ed7000a01dcce37a3b29
EDCA <eric.cavanzo@outlook.com>
2024-11-14 21:53:59 -0500

    Primera sesion de JS

---
+++ holaMundo/clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = "Woody"
+console.log(cajaDeAndy)
+
+//Lo no permitido, todo lo que sean ambiguedades o asignaciones que no aporten para saber de que tratan o con que
+//proposito pudieron haberse creado o su funcionalidad llevando a futuro errores.
+let c = "woody"
+let cda = "woody"
+let pcAndy = "Woody"
+
+//Lo permitido, todo lo que son nombres descriptivos o asignicaciones con nombres o abreviaciones que 
+///sean conocidas por el entorno comun y no vayan a generar confusiones
+let primerTrasteoDeAndy = "Woody"
+let urlDelUsuario = "https://www.google.com"
+let idDelUsuario ="123456789"
